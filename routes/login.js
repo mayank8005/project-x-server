81a1e4e3d2d4c010f376f84d28e05ad317f58fc3
mayank agrawal <mayank8005@gmail.com>
2018-03-19 15:21:01 +0530

    removed auth require form login ---minor change

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const authenticator = require('../project_modules/Authentication');
 const router = express.Router();
-
-//creating authenticator object
 
 router.get('/login', function(req, res, next){
 
