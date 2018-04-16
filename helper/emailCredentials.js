ffecb600334bf27c97f1b7ac41f35625692ecdce
mayank agrawal <mayank8005@gmail.com>
2018-04-16 20:14:01 +0530

    final prototype

--- helper/emailCredentials.js
+++ helper/emailCredentials.js
@@ -1,9 +1,13 @@
 const nodemailer = require('nodemailer');
+const  smtpPool = require('nodemailer-smtp-pool');
 
-module.exports.transporter = nodemailer.createTransport({
+
+module.exports.transporter = nodemailer.createTransport(smtpPool({
     service: 'gmail',
     auth: {
-        user: 'itrack2k18@gmail.com',
+        user: 'itrackteam2k18',
         pass: 'itrack1234'
-    }
-});
+},
+maxConnections: 5,
+    maxMessages: 10
+}));
